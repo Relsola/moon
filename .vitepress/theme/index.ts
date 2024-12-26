b52e8622c4d4f6099ab9691fa9a4617e2b697516
Relsola <sola001018@gmail.com>
2024-12-26 23:27:10 +0800

    新增 Element Plus 图标支持

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,14 @@
+import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import 'virtual:uno.css';
 
-export default DefaultTheme;
+export default {
+  extends: DefaultTheme,
+
+  enhanceApp({ app }) {
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(key, component);
+    }
+  }
+} satisfies Theme;
