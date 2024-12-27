0abcea5f01b1c6a117028373c7b8a6c69b0c7446
Relsola <sola001018@gmail.com>
2024-12-27 23:48:19 +0800

    新增正则表达式组件功能，支持修饰符选择和匹配结果显示

--- components/Regex/index.vue
+++ components/Regex/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
-const regex = ref('');
+/** 正则表达式文本 */
+const regexText = ref('');
+/** 修饰符 */
 const modifier = ref(['g']);
-
+/** 进行匹配的文本 */
+const text = ref('');
+/** 修饰符列表 */
 const modifierList = [
   { value: 'g', label: '全局匹配 -g' },
   { value: 'i', label: '忽略大小写 -i' },
@@ -12,10 +16,32 @@ const modifierList = [
   { value: 'u', label: 'unicode -u' },
   { value: 'y', label: '粘连匹配 -y' }
 ];
+
+/** 正则表达式 */
+const reg = computed(() => {
+  try {
+    return new RegExp(regexText.value, modifier.value.join(''));
+  } catch (e) {
+    return null;
+  }
+});
+
+/** 匹配结果 */
+const matchText = computed(() => {
+  // 待匹配文本为空
+  if (!text.value) return '<span class="text-slate-400">匹配结果...</span>';
+  // 正则表达式为空
+  if (!reg.value) return '<span class="text-red-500">无效的正则表达式</span>';
+
+  return text.value.replace(
+    reg.value,
+    match => `<span class="bg-blue">${match}</span>`
+  );
+});
 </script>
 
 <template>
-  <el-input v-model="regex" style="width: 60%" placeholder="请输入正则表达式">
+  <el-input v-model="regexText" style="width: 60%" placeholder="请输入正则表达式">
     <template #prepend> / </template>
     <template #append> /{{ modifier.join('') }} </template>
   </el-input>
@@ -30,7 +56,7 @@ const modifierList = [
       />
     </el-checkbox-group>
 
-    <div class="my2" style="border-top: 1px solid var(--el-border-color)"></div>
+    <el-divider style="margin: 5px 0" />
     <el-link
       href="https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Guide/Regular_expressions#%E9%80%9A%E8%BF%87%E6%A0%87%E5%BF%97%E8%BF%9B%E8%A1%8C%E9%AB%98%E7%BA%A7%E6%90%9C%E7%B4%A2"
       target="_blank"
@@ -48,4 +74,16 @@ const modifierList = [
       <el-button class="ml2">语法参考</el-button>
     </template>
   </el-popover>
+
+  <el-input
+    v-model="text"
+    class="mt5"
+    type="textarea"
+    placeholder="在此输入待匹配文本"
+  />
+
+  <div
+    v-html="matchText"
+    class="mt5 w100% border-solid border border-slate-300 rounded p2 text-xs min-h-12"
+  ></div>
 </template>
