e9adc43a66d1a4f5a0fc9ea096bb39b5515fc595
Relsola <sola001018@gmail.com>
2025-01-01 23:24:01 +0800

    新增链接组件，更新前端和后端分类数据

--- components/Link/index.vue
+++ components/Link/index.vue
@@ -1,26 +1,19 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
-import type { TabsPaneContext } from 'element-plus';
+import data from './data.json';
 
 const activeName = ref('web');
-
-const handleClick = (tab: TabsPaneContext, event: Event) => {
-  console.log(tab, event);
-};
 </script>
 
 <template>
-  <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="Web 前端" name="web">
-      Web
-      <!-- >
-      [JS正则表达式完整教程](https://juejin.cn/post/6844903487155732494?searchId=202412221741358057531E480441E6BB82)
-      >
-      [正则表达式字符详解](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Guide/Regular_expressions) -->
+  <el-tabs v-model="activeName">
+    <el-tab-pane v-for="{ label, name, data } in data" :label="label" :name="name">
+      <a v-for="{ name, href } in data" :href="href" target="_blank">
+        <el-card class="inline-block m2 w80" shadow="hover">
+          {{ name }}
+        </el-card>
+      </a>
     </el-tab-pane>
-    <el-tab-pane label="Java 后端" name="java">Java</el-tab-pane>
-    <el-tab-pane label="GitHub" name="github">GitHub</el-tab-pane>
-    <el-tab-pane label="Other" name="other">Other</el-tab-pane>
   </el-tabs>
 
   <el-backtop :right="100" :bottom="100" />
