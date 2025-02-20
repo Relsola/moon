7337668c90b24acfedbe4f6d30727f43394b3ca6
Relsola <sola001018@gmail.com>
2025-02-20 22:46:05 +0800

    清理项目

--- Example/novel-web/src/api/news.js
+++
@@ -1,9 +0,0 @@
-import request from '../utils/request'
-
-export function listLatestNews() {
-    return request.get('/front/news/latest_list');
-}
-
-export function getNewsById(newsId) {
-    return request.get(`/front/news/${newsId}`);
-}
--- Example/novel-web/src/api/resource.js
+++
@@ -1,5 +0,0 @@
-import request from '../utils/request'
-
-export function getImgVerifyCode() {
-    return request.get('/front/resource/img_verify_code');
-}
