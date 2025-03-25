a6dfa4a6fb60f1b81fcb499c24be3d7b8bce9cfa
Relsola <sola001018@gmail.com>
2025-03-26 00:11:15 +0800

    更新导航菜单和侧边栏，新增游戏记录章节及相关文档

--- .vitepress/nav.ts
+++ .vitepress/nav.ts
@@ -10,44 +10,44 @@ export default [
 	{
 		text: 'Frontend',
 		items: [
-			{ text: '总论', link: '' },
+			{ text: '总论', link: '/kt' },
 			{ text: 'ES6+', link: '/frontend/es6/block-scope' },
 			{ text: 'TypeScript', link: '/frontend/typescript/data-types' },
-			{ text: 'TS 类型挑战', link: '' },
-			{ text: 'Web API', link: '' }
+			{ text: 'TS 类型挑战', link: '/kt' },
+			{ text: 'Web API', link: '/kt' }
 		]
 	},
 	{
 		text: 'Backend',
 		items: [
-			{ text: 'Java 知识体系', link: '' },
-			{ text: '常用框架', link: '' },
-			{ text: '数据库', link: '' },
-			{ text: '系统设计', link: '' },
-			{ text: '分布式', link: '' }
+			{ text: 'Java 知识体系', link: '/kt' },
+			{ text: '常用框架', link: '/kt' },
+			{ text: '数据库', link: '/kt' },
+			{ text: '系统设计', link: '/kt' },
+			{ text: '分布式', link: '/kt' }
 		]
 	},
 	{
 		text: 'Computer',
 		items: [
-			{ text: '数据结构与算法', link: '' },
-			{ text: '操作系统', link: '' },
-			{ text: '编译原理', link: '' },
-			{ text: 'Linux', link: '' },
-			{ text: 'Shell', link: '' },
-			{ text: 'Docker', link: '' },
-			{ text: 'Git', link: '' }
+			{ text: '数据结构与算法', link: '/kt' },
+			{ text: '操作系统', link: '/kt' },
+			{ text: '编译原理', link: '/kt' },
+			{ text: 'Linux', link: '/kt' },
+			{ text: 'Shell', link: '/kt' },
+			{ text: 'Docker', link: '/kt' },
+			{ text: 'Git', link: '/kt' }
 		]
 	},
 	{
 		text: 'Interest',
 		items: [
-			{ text: '游戏记录', link: '' },
-			{ text: 'C++ 开发', link: '' },
-			{ text: 'C# 开发', link: '' },
-			{ text: 'Godot 开发', link: '' },
-			{ text: '概率论', link: '' },
-			{ text: '离散数学', link: '' }
+			{ text: '游戏记录', link: '/interest/record/summer-pockets' },
+			{ text: 'C++ 开发', link: '/kt' },
+			{ text: 'C# 开发', link: '/kt' },
+			{ text: 'Godot 开发', link: '/kt' },
+			{ text: '概率论', link: '/kt' },
+			{ text: '离散数学', link: '/kt' }
 		]
 	},
 	{ text: 'Link', link: '/link/' },
